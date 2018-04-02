2e8ca04fc212ed225289bce9b60bc50875db8eb6
Forest Anderson <wizcardforest@gmail.com>
2018-04-02 14:07:40 -0400

    Site loads, but problems when submitting

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+from passwordGen import genPassword
  
 # App config.
 DEBUG = True
@@ -9,11 +11,13 @@ app.config.from_object(__name__)
 #Allows server to be hosted externally
 app.run(host='0.0.0.0')
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+app.jinja_env.globals.update(genPassword=genPassword)
  
 class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
+    hooli = TextField('New Hooli Password:', validators=[validators.required(), validators.EqualTo('Hooliv', message='Hooli Passwords must match')])
+    piper = TextField('New Pied Piper Password:', validators=[validators.required(), validators.EqualTo('Piperv', message='Pied Piper Passwords must match')])
+    myspace = TextField('New Myspace Password:', validators=[validators.required(), validators.EqualTo('MyspaceV', message='Myspace Passwords must match')])
  
     def reset(self):
         blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
@@ -21,10 +25,9 @@ class ReusableForm(Form):
  
  
 @app.route("/", methods=['GET', 'POST'])
-def hello():
-    print "test"
+def form():
     form = ReusableForm(request.form)
- 
+
     print form.errors
     if request.method == 'POST':
         name=request.form['name']
@@ -36,7 +39,7 @@ def hello():
         else:
             flash('All the form fields are required. ')
  
-    return render_template('hello.html', form=form)
+    return render_template('password.html', form=form, p1=genPassword(), p2=genPassword(), p3=genPassword())
  
 if __name__ == "__main__":
     app.run()
